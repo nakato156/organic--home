7e5478b8e8facd4833a96644769edfc74f03dbdd
Christian <christianvelasces@gmail.com>
2020-05-25 12:11:26 -0500

    agregado metodo de pago

---
+++ modulo/principal.php
@@ -0,0 +1,20 @@
+<div class="oh">
+	<h2>Organic Home</h2>
+	<center>
+		<div class="fondoh">
+			<img src="./img/ohicon1.png" class="imgoh">
+		</div>
+	</center>
+</div>
+<center>
+	<span>
+		En Orgnic Home nos preocupamos par la salud y bienestar de las personas, por esta razon Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+	</span>
+</center>
+<center>
+<div class="redes">
+	<a herf="" class="icon-blog"></a>
+</div>
+</center>
